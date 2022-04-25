9cd75a4dcbfb506e5a430eaa23b01c9b5e2ebecd
Tiemenerikjansen <44866188+Tiemenerikjansen@users.noreply.github.com>
2022-04-25 22:51:23 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,16 +11,18 @@ sudo apt -y install \
 echo Creating dotfiles directory...
 mkdir -p ~/.dotfiles
 mkdir -p ~/.config/nvim
-
-
+touch .config/nvim/init.vim
 
 git clone https://github.com/Tiemenerikjansen/Dotfiles.git ~/.dotfiles
+
+
+sudo apt install zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+chsh -s $(which zsh)
 
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/init.vim ~/.config
 
-sudo apt install zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-chsh -s $(which zsh)
 zsh
 
