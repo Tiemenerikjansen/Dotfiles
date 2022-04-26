2883a133c0e924ee63b5ca3c1b6e1a783e149385
Tiemenerikjansen <44866188+Tiemenerikjansen@users.noreply.github.com>
2022-04-26 12:07:27 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,12 +5,14 @@ sudo apt -y install \
   neovim \
   exa \
   tree \
+  neofetch
   curl
   
 
 echo Creating dotfiles directory...
 mkdir -p ~/.dotfiles
 mkdir -p ~/.config/nvim
+
 touch .config/nvim/init.vim
 
 git clone https://github.com/Tiemenerikjansen/Dotfiles.git ~/.dotfiles
@@ -19,10 +21,9 @@ git clone https://github.com/Tiemenerikjansen/Dotfiles.git ~/.dotfiles
 sudo apt install zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-chsh -s $(which zsh)
 
 ln -sf ~/.dotfiles/.zshrc ~/.zshrc
 ln -sf ~/.dotfiles/init.vim ~/.config
-
+chsh -s $(which zsh)
 zsh
 
